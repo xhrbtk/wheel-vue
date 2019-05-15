3d02bc24e07752dfa4875bfd6d1860745f2033e1
hongrui.xu <xuhongrui@xuhongrui.lan>
2019-05-15 22:44:19 +0800

    基本实现row和col的功能

--- src/row.vue
+++ src/row.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
    <slot></slot>
   </div>
 </template>
 <script>
 export default {
-
+  name: 'WheelRow',
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
+  mounted() {
+    // 父子传值
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  }
 }
 </script>
 <style lang="scss" scoped>
 .row{
     display: flex;
+    // margin: 0 -10px;
   }
 </style>
 
